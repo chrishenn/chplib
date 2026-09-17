dbe21d8f47ba9ff386ac91b842ae994a3fba595f
chrishenn <chenn@alum.mit.edu>
2026-09-16 21:42:57 -0500

    feat: time

--- chplib/time.ps1
+++ chplib/time.ps1
@@ -1,4 +1,18 @@
+. $psscriptroot\types.ps1
+
 function time_sync {
+    # requires time_sync_enable; sets the setting 'set time automatically' and gates the w32tm service
     start-service w32time
     w32tm /resync /force
+}
+
+function time_sync_enable {
+    # set to "NoSync" to disable
+    $key = 'HKLM:\SYSTEM\CurrentControlSet\Services\W32Time\Parameters'
+    rprop $key 'Type' 'String' 'NTP'
+    svc_startup w32time ([Start]::automatic)
+}
+
+function time_tz_auto {
+    svc_startup tzautoupdate ([Start]::automatic)
 }
